15f7520a5b1eb055a184c07ab38ac531711b2515
NymScript <niyimorera28@gmail.com>
2024-08-21 17:54:32 -0500

    mitad de depedencias inico project

---
+++ lib/views/home/home_view.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+
+      //
+      floatingActionButton: Material(
+        borderRadius: BorderRadius.circular(15),
+        elevation: 10,
+        child: Container(
+          width: 70,
+          height: 70,
+          decoration: BoxDecoration(
+            //color: AppColors.primaryColor,
+            borderRadius: BorderRadius.circular(15)),
+          child: Icon(
+            Icons.add,
+            color: Colors.white,
+            ),
+          ),
+        ),
+    );
+  }
+}
